864459abc968d2d257ae499d77ff27d75d559fec
8Crafter (PC) <8crafteryt@gmail.com>
2025-10-29 17:21:00 -0700

    Updated

--- docs/andexdb/versions.js
+++ docs/andexdb/versions.js
@@ -1,6 +1,12 @@
 "use strict"
 export const DOC_VERSIONS = [
 	'stable',
+	'v1.41.0',
+	'v1.40.3',
+	'v1.40.2',
+	'v1.40.1',
+	'v1.40.0',
+	'v1.39.0',
 	'v1.38.0',
 	'v1.37.1',
 	'v1.37.0',
@@ -15,4 +21,5 @@ export const DOC_VERSIONS = [
 	'v1.33.1',
 	'v1.33.1-preview.20',
 	'v1.33.0',
+	'v1.5.4',
 ];
